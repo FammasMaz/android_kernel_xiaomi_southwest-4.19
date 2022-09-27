1e82096610536610a5c9d46b0c35f99347df8ad7
Moeeze Hassan <fammas.maz@gmail.com>
2022-09-27 06:41:31 +0200

    defconfig: regen
    Signed-off-by: Moeeze Hassan <fammas.maz@gmail.com>

--- build.sh
+++ build.sh
@@ -40,7 +40,7 @@ function finerr() {
 }
 # Compile plox
 function compile() {
-    make O=out ARCH=arm64 sdm660-perf_defconfig
+    make O=out ARCH=arm64 lavender_defconfig
     make -j$(nproc --all) O=out \
                     ARCH=arm64 \
                     CC=clang \
